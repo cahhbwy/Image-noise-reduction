4a0366ce6dd03f8edddb106cf653048c90b5821f
cahhbwy <cahhbwy@mail.ustc.edu.cn>
2017-12-25 23:47:35 +0800

    2017年12月25日 23点47分

--- data_IO.py
+++ data_IO.py
@@ -6,6 +6,42 @@ import os
 from PIL import Image
 import random
 import numpy
+import multiprocessing
+
+
+def create_tfrecord(_filename, _block_size, _data_block, _from_path="image", _save_path="data", _mode="RGB"):
+    image_list = os.listdir(_from_path)
+    len_image_list = len(image_list)
+    if _mode is "RGB":
+        channel = 3
+    elif _mode is "L":
+        channel = 1
+    else:
+        return
+    writer = tf.python_io.TFRecordWriter(os.path.join(_save_path, _filename))
+    count = 0
+    while count < _data_block:
+        image_index = random.randint(0, len_image_list - 1)
+        try:
+            image = Image.open(os.path.join(_from_path, image_list[image_index]))
+        except OSError:
+            continue
+        if image.size[0] < _block_size or image.size[1] < _block_size:
+            continue
+        if image.mode is not _mode:
+            image = image.convert(_mode)
+        x = random.randint(0, image.size[0] - _block_size)
+        y = random.randint(0, image.size[1] - _block_size)
+        image_bytes = image.crop((x, y, x + _block_size, y + _block_size)).tobytes()
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'block_size': tf.train.Feature(int64_list=tf.train.Int64List(value=[_block_size])),
+            'channel': tf.train.Feature(int64_list=tf.train.Int64List(value=[channel])),
+            'image_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_bytes]))
+        }))
+        writer.write(example.SerializeToString())
+        count += 1
+    print("create {} finished.".format(_filename))
+    writer.close()
 
 
 def create_tfrecords(block_size=128, total=65536, mode='RGB', from_path="image", save_path="data"):
@@ -23,7 +59,10 @@ def create_tfrecords(block_size=128, total=65536, mode='RGB', from_path="image",
     writer = tf.python_io.TFRecordWriter(os.path.join(save_path, "%04d.tfrecords" % 0))
     while count < total:
         image_index = random.randint(0, len_image_list - 1)
-        image = Image.open(os.path.join(from_path, image_list[image_index]))
+        try:
+            image = Image.open(os.path.join(from_path, image_list[image_index]))
+        except OSError:
+            continue
         if image.size[0] < block_size or image.size[1] < block_size:
             continue
         if image.mode is not mode:
@@ -45,6 +84,25 @@ def create_tfrecords(block_size=128, total=65536, mode='RGB', from_path="image",
     writer.close()
 
 
+def create_tfrecords_multi_threads(block_size=128, total=65536, file_size=192, mode='RGB', from_path="image", save_path="data", limit_threads_num=None):
+    if limit_threads_num is None:
+        pool = multiprocessing.Pool()
+    else:
+        pool = multiprocessing.Pool(limit_threads_num)
+    if mode is "RGB":
+        channel = 3
+    elif mode is "L":
+        channel = 1
+    else:
+        return
+    data_block = (file_size * 1024 * 1024) // (block_size * block_size * channel)
+    count = total // data_block
+    params = []
+    for i in range(count):
+        params.append(("%04d.tfrecords" % i, block_size, data_block, from_path, save_path, mode))
+    pool.starmap(create_tfrecord, params)
+
+
 def read_tfrecords(dir_path="data", block_size=128, channel=3):
     filename_list = [os.path.join(dir_path, filename) for filename in os.listdir(dir_path)]
     filename_queue = tf.train.string_input_producer(filename_list)
@@ -59,12 +117,19 @@ def read_tfrecords(dir_path="data", block_size=128, channel=3):
     return tf.reshape(data, (block_size, block_size, channel))
 
 
-def sample_save(noise, noisy_image, image, output_clear_image, save_path, block_size=128):
+def sample_save(noise, noisy_image, image, output_clear_image, save_path, block_size=128, channel=3):
+    if channel == 1:
+        noise = numpy.reshape(noise, (block_size, block_size))
+        noisy_image = numpy.reshape(noisy_image, (block_size, block_size))
+        image = numpy.reshape(image, (block_size, block_size))
+        output_clear_image = numpy.reshape(output_clear_image, (block_size, block_size))
+        output = Image.new("L", (block_size * 2, block_size * 2))
+    else:
+        output = Image.new("RGB", (block_size * 2, block_size * 2))
     img_noise = Image.fromarray(numpy.clip(numpy.add(numpy.multiply(noise, 255), 127).astype(numpy.uint8), 0, 255))
     img_noisy_image = Image.fromarray(numpy.multiply(noisy_image, 255).astype(numpy.uint8))
     img_image = Image.fromarray(numpy.multiply(image, 255).astype(numpy.uint8))
     img_output_clear_image = Image.fromarray(numpy.multiply(output_clear_image, 255).astype(numpy.uint8))
-    output = Image.new("RGB", (block_size * 2, block_size * 2))
     output.paste(img_noise, (0, 0, block_size, block_size))
     output.paste(img_noisy_image, (block_size, 0, block_size * 2, block_size))
     output.paste(img_image, (0, block_size, block_size, block_size * 2))
@@ -73,4 +138,5 @@ def sample_save(noise, noisy_image, image, output_clear_image, save_path, block_
 
 
 if __name__ == '__main__':
-    create_tfrecords(128, 8192, "RGB", "image", "data")
+    # create_tfrecords(128, 65536, "L", "image", "data")
+    create_tfrecords_multi_threads(128, 262144, 256, 'L', "image", "data")
--- model_0.py
+++ model_0.py
@@ -1,49 +1,91 @@
 # coding:utf-8
 # 模型: 0
 
-from utils import *
 from data_IO import *
 import tensorflow as tf
+from tensorflow.contrib import layers
+import numpy as np
 
 
-def model(_image, _batch_size, _block_size, _channel):
-    noise_stddev = tf.random_uniform([_batch_size, 1, 1, 1], 0.0, 0.3, name="noise_stddev")
-    influence_range = tf.random_uniform([_batch_size, 1, 1, 1], 0.0, 1.0, name="influence_range")
-    influence = tf.cast(tf.greater(tf.random_uniform([_batch_size, _block_size, _block_size, _channel], 0.0, 1.0), influence_range), tf.float32)
-    noise = tf.random_normal([_batch_size, _block_size, _block_size, _channel], 0.0, noise_stddev, name="noise")
-    noise = tf.multiply(noise, influence)
+def model(_image, _batch_size, _block_size):
+    noise_stddev = tf.random_uniform([_batch_size, 1, 1, 1], 0.015, 0.1, name="noise_stddev")
+    noise = tf.random_normal([_batch_size, _block_size, _block_size, 1], 0.0, noise_stddev, name="noise")
     clear_image = tf.divide(tf.cast(_image, tf.float32), 255.0, "clear_image")
     noisy_image = tf.clip_by_value(clear_image + noise, 0.0, 1.0, "noisy_image")
-    h_0 = conv2d(noisy_image, 16, 5, 5, (1, 1, 1, 1), "conv2d_0")
-    h_1 = lrelu(tf.contrib.layers.layer_norm(conv2d(h_0, 48, 5, 5, (1, 1, 1, 1), "conv2d_1"), scope="ln_1"))
-    h_2 = lrelu(tf.contrib.layers.layer_norm(conv2d(h_1, 96, 5, 5, (1, 1, 1, 1), "conv2d_2"), scope="ln_2"))
-    h_3 = lrelu(tf.contrib.layers.layer_norm(conv2d(h_2, 36, 5, 5, (1, 1, 1, 1), "conv2d_3"), scope="ln_3"))
-    output_noise = tf.contrib.layers.layer_norm(conv2d(h_3, _channel, 5, 5, (1, 1, 1, 1), "conv2d_4"), scope="ln_4")
+    regularizer = tf.contrib.layers.l2_regularizer(0.01)
+    h_0 = layers.conv2d(noisy_image, 24, 5, activation_fn=None, weights_regularizer=regularizer, biases_regularizer=regularizer)
+    h_1 = layers.conv2d(h_0, 48, 5, activation_fn=tf.nn.leaky_relu, normalizer_fn=tf.contrib.layers.layer_norm, weights_regularizer=regularizer, biases_regularizer=regularizer)
+    h_2 = layers.conv2d(h_1, 24, 5, activation_fn=tf.nn.leaky_relu, normalizer_fn=tf.contrib.layers.layer_norm, weights_regularizer=regularizer, biases_regularizer=regularizer)
+    output_noise = layers.conv2d(h_2, 1, 5, activation_fn=None, weights_regularizer=regularizer, biases_regularizer=regularizer)
     output_clear_image = noisy_image - output_noise
-    diff = output_clear_image - clear_image
-    loss = tf.reduce_mean(tf.pow(diff, 2) + tf.abs(diff) + tf.multiply(tf.multiply(tf.abs(diff), tf.cast(tf.less(influence, 0.5), tf.float32)), 2.0))
+    loss = tf.losses.mean_squared_error(output_clear_image, clear_image) + tf.contrib.layers.apply_regularization(regularizer, tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
     return loss, noise, noisy_image, output_noise, clear_image, output_clear_image
 
 
-if __name__ == '__main__':
-    batch_size = 64
-    block_size = 128
-    channel = 3
-    image = read_tfrecords("data", block_size, channel)
-    image = tf.train.shuffle_batch([image], batch_size, capacity=2000, min_after_dequeue=1000)
-    m_loss, m_noise, m_noisy_image, m_output_noise, m_clear_image, m_output_clear_image = model(image, batch_size, block_size, channel)
+def train(_batch_size, _block_size, _channel, step_start=0, restore=False):
+    image = read_tfrecords("data", _block_size, _channel)
+    image = tf.train.shuffle_batch([image], _batch_size, capacity=2000, min_after_dequeue=1000)
+    m_loss, m_noise, m_noisy_image, m_output_noise, m_clear_image, m_output_clear_image = model(image, _batch_size, _block_size)
     global_step = tf.Variable(0, trainable=False)
-    learning_rate = tf.train.exponential_decay(learning_rate=0.0001, global_step=global_step, decay_steps=100, decay_rate=0.90)
+    learning_rate = tf.train.exponential_decay(learning_rate=0.0005, global_step=global_step, decay_steps=100, decay_rate=0.90)
     op = tf.train.AdamOptimizer(learning_rate).minimize(m_loss)
-    sess = tf.InteractiveSession()
+    sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     tf.train.start_queue_runners(sess=sess)
     saver = tf.train.Saver(max_to_keep=10)
-    for step in range(100000):
+    if restore:
+        saver.restore(sess, "./model/model.ckpt-%d" % step_start)
+    for step in range(step_start, 100000):
         _, v_loss = sess.run([op, m_loss], feed_dict={global_step: step})
         if step % 10 == 0:
             print("step %6d: loss = %f" % (step, v_loss))
         if step % 100 == 0:
             v_noise, v_noisy_image, v_clear_image, v_output_clear_image = sess.run([m_noise, m_noisy_image, m_clear_image, m_output_clear_image])
-            sample_save(v_noise[0], v_noisy_image[0], v_clear_image[0], v_output_clear_image[0], "sample/%04d.jpg" % step, block_size)
+            sample_save(v_noise[0], v_noisy_image[0], v_clear_image[0], v_output_clear_image[0], "sample/%04d.jpg" % step, _block_size, 1)
             saver.save(sess, "./model/model.ckpt", global_step=step)
+
+
+def test(_image):
+    def test_model():
+        noisy_image_input = tf.placeholder(tf.uint8, [None, None, None, 1])
+        noisy_image = tf.divide(tf.cast(noisy_image_input, tf.float32), 255.)
+        h_0 = layers.conv2d(noisy_image, 24, 5, activation_fn=None)
+        h_1 = layers.conv2d(h_0, 48, 5, activation_fn=tf.nn.leaky_relu, normalizer_fn=tf.contrib.layers.layer_norm)
+        h_2 = layers.conv2d(h_1, 24, 5, activation_fn=tf.nn.leaky_relu, normalizer_fn=tf.contrib.layers.layer_norm)
+        output_noise = layers.conv2d(h_2, 1, 5, activation_fn=None)
+        output_clear_image = noisy_image - output_noise
+        return noisy_image_input, output_clear_image
+
+    if _image.mode not in ["RGB", "L"]:
+        _image = _image.convert("RGB")
+    img_data = np.array(_image).transpose((2, 0, 1))[..., np.newaxis]
+    m_noisy_image_input, m_output_clear_image = test_model()
+    sess = tf.InteractiveSession()
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
+    saver.restore(sess, "./model/model.ckpt-10700")
+    v_output_clear_image = sess.run(m_output_clear_image, feed_dict={m_noisy_image_input: img_data})
+    out_image = v_output_clear_image.reshape(v_output_clear_image.shape[0:3]).transpose((1, 2, 0))
+    if out_image.shape[2] == 1:
+        out_image = out_image.reshape(out_image.shape[0:2])
+    out_image = np.multiply(out_image, 255.).astype(np.uint8)
+    return Image.fromarray(out_image)
+
+
+def add_noise(_image):
+    img_data = np.divide(np.array(_image).astype(np.float32), 255.)
+    noise_stddev = np.random.uniform(0.015, 0.1)
+    noise = np.random.normal(0.0, noise_stddev, img_data.shape)
+    return Image.fromarray(np.multiply(np.add(img_data, noise), 255.).astype(np.uint8))
+
+
+if __name__ == '__main__':
+    batch_size = 64
+    block_size = 128
+    channel = 1
+    train(batch_size, block_size, channel)
+    # image = Image.open("test/test.jpg")
+    # noisy_image = add_noise(image)
+    # output = test(noisy_image)
+    # noisy_image.save("test/noisy_image.jpg")
+    # output.save("test/output.jpg")
